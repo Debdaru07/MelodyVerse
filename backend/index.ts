fd52225889bec8230d75ee364c56989a6b480a56
shayanroychoudhury <shayan.roy31@gmail.com>
2025-02-11 04:23:17 +0530

    add rate limiter

--- backend/index.ts
+++ backend/index.ts
@@ -7,10 +7,22 @@ import { verifyEmail } from './routes/verifyEmail';
 import { verifyJWT } from './middleware/verifyToken';
 import cors from 'cors';
 import { resetPassword } from './routes/resetEmail';
+
 const app = express();
+
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per window
+    message: "Too many requests from this IP, please try again later.",
+    headers: true, // Include rate limit headers
+});
+
 
 app.use(express.json());
 app.use(cors())
+app.use(limiter);
 
 // @ts-expect-error
 app.post('/signup', signUp);
